d784711b0ce71ddf5c00f3b0a9ed7ec5cfe93b48
Plamen <biznetmen@gmail.com>
2022-09-06 18:38:16 +0300

    adding hello task with times parameter

--- hardhat.config.js
+++ hardhat.config.js
@@ -59,3 +59,16 @@ task(
     }
   }
 );
+
+task("hello", "Prints 'Hello' multiple times")
+  .addOptionalParam(
+    "times",
+    "The number of times to print 'Hello'",
+    1,
+    types.int
+  )
+  .setAction(async ({ times }) => {
+    for (let i = 0; i < times; i++) {
+      console.log("Hello");
+    }
+  });
